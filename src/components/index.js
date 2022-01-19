74808be670e0cef6c81f50bfc40133bbb1dc0cd6
mehran vejdani <mehran.vejdanii@gmail.com>
2022-01-19 17:54:08 +0330

    add components and library

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Todo from "./todo/Todo";
+import Todos from "./todos/Todos"
+
+export { Todo, Todos }
---
+++ src/components/todos/Todos.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Todos = () => {
+  return (
+    <div>
+      <h2>this is todos</h2>
+    </div>
+  );
+};
+
+export default Todos;
